0c776165d2848e702de297f74ae6d92d51cca46b
Jordan <51269322+jordann124@users.noreply.github.com>
2019-06-03 20:47:34 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,66 @@
+Locales['en'] = {
+  -- Cloakroom
+  ['cloakroom'] = 'Vestuário',
+  ['ems_clothes_civil'] = 'Roupa casual',
+  ['ems_clothes_ems'] = 'Uniforme paramédico',
+  -- Vehicles
+  ['veh_menu'] = 'Veículo',
+  ['veh_spawn'] = 'Pressione ~INPUT_CONTEXT~ para escolher veículo ou helicoptero',
+  ['store_veh'] = 'Pressione ~INPUT_CONTEXT~ para guardar o veículo',
+  ['ambulance'] = 'Ambulância',
+  ['ambulancei'] = 'Carrinha',
+  ['ambulancec'] = 'Mercedes',
+  ['ems_gs1200'] = 'Moto',
+  ['helicopter'] = 'Helicoptero',
+  -- Action Menu
+  ['hospital'] = 'Hospital',
+  ['revive_inprogress'] = 'Reanimação em progresso',
+  ['revive_complete'] = 'Você reviveu',
+  ['no_players'] = 'Nenhum jogador nas proximidades',
+  ['no_vehicles'] = 'Nenhum veículo nas proximidades',
+  ['isdead'] = 'está morto',
+  ['unconscious'] = 'Não está inconsciente',
+  -- Boss Menu
+  ['deposit_society'] = 'eMS Depositou',
+  ['withdraw_society'] = 'eMS Levantou',
+  ['boss_actions'] = 'Ações de chefe',
+  -- Misc
+  ['invalid_amount'] = '~r~quantidade inválida',
+  ['open_menu'] = 'Pressione ~INPUT_CONTEXT~ para abrir o menu',
+  ['deposit_amount'] = 'Quantidade para deposito',
+  ['money_withdraw'] = 'Quantidade para levantar',
+  ['fast_travel'] = 'Pressione ~INPUT_CONTEXT~ para viagem rápida.',
+  ['open_pharmacy'] = 'Pressione ~INPUT_CONTEXT~ para abrir a farmácia.',
+  ['pharmacy_menu_title'] = 'Farmácia',
+  ['pharmacy_take'] = 'tirar',
+  ['medikit'] = 'medkit',
+  ['bandage'] = 'ligaduras',
+  ['max_item'] = 'já tens o máximo desse item.',
+  -- F6 Menu
+  ['ems_menu'] = 'eMS Menu',
+  ['ems_menu_title'] = 'Paramédico - EMS Menu',
+  ['ems_menu_revive'] = 'Reviver jogador',
+  ['ems_menu_putincar'] = 'Colocar no veículo',
+  ['ems_menu_small'] = 'curar pequenas feridas',
+  ['ems_menu_big'] = 'curar feridas graves',
+  -- Phone
+  ['alert_ambulance'] = 'ambulância de serviço',
+  -- Death
+  ['respawn_at_hospital'] = 'Queres ser transportado para o hospital?',
+  ['yes'] = 'sim',
+  ['please_wait'] = 'por favor, espere ~b~',
+  ['minutes'] = ' minutos ',
+  ['seconds_fine'] = ' segundos ~w~para respawn \nRespawn agora para ~g~$',
+  ['seconds'] = ' segundos ~w~para respawn.',
+  ['press_respawn_fine'] = '~w~ [Pressione ~b~E~w~]',
+  ['press_respawn'] = 'pressione [~b~E~w~] para respawn.',
+  -- Revive
+  ['revive_help'] = 'reviver um jogador',
+    -- Item
+  ['used_medikit'] = 'Usaste 1x medkit',
+  ['used_bandage'] = 'Usaste 1x ligadura',
+  ['not_enough_medikit'] = 'não tens ~b~medkits~w~.',
+  ['not_enough_bandage'] = 'não tens ~b~ligaduras~w~.',
+  ['healed'] = 'foste tratado.',
+   -- Misc
+}
